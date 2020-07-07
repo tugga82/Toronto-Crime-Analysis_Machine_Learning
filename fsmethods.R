ac5ed41a93e4c877e0ccf5c48cd78a813989ec5a
Vidyasankar Sundar <38926820+tugga82@users.noreply.github.com>
2020-07-06 23:50:32 -0400

    Add files via upload

---
+++ fsmethods.R
@@ -0,0 +1,28 @@
+library(FSelector)
+set.seed(456)
+#Using information gain filter on all the variables in the dataset did not provide appropriate variables for the model. Therefore, manually drop the variables that are not appropriate or relevant and then run the information gain to identify attributes that are important.  
+ind1<-sample(2, nrow(EDAfilter), replace = TRUE, prob=c(0.7,0.3))
+featureselect<-EDAfilter[ind1 == 1,]
+fstest<-EDAfilter[ind1 == 2,]
+weights <- information.gain(MCI~., featureselect)
+print(weights)
+subset <- cutoff.k(weights, 2)
+f <- as.simple.formula(subset, "MCI")
+print(f)
+#Applying information gain on the dataset after manually dropping several variables resulted in the following variables for applying the models. This includes premisetype, occurrencehour, division, neighbourhood, longitude and lattitude. 
+set.seed(123)
+ind2<-sample(2, nrow(EDAdrop), replace = TRUE, prob=c(0.7,0.3))
+featureselect2<-EDAdrop[ind1 == 1,]
+fstest2<-EDAdrop[ind1 == 2,]
+weights2 <- information.gain(MCI~., featureselect2)
+print(weights2)
+subset2 <- cutoff.k(weights2, 2)
+f2 <- as.simple.formula(subset2, "MCI")
+print(f2)
+#Run this on the 30% partitioned dataset and see how the information gain filter works
+weights3 <- information.gain(MCI~., fstest2)
+print(weights3)
+subset3 <- cutoff.k(weights3, 2)
+f3 <- as.simple.formula(subset3, "MCI")
+print(f3)
+#This run on the partitioned dataset also resulted in premisetype, occurrencehour, division, neighbourhood, longitude and lattitude as the most important variables. 
